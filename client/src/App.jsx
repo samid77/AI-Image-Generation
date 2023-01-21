cbfa8596ebf1b93b2a8f29e7d012fa0556775444
Samid77 <Dimas@Inass-Mac-Air.local>
2023-01-21 16:09:48 +0700

    Setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
