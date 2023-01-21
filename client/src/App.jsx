91fc7db279fd2cf04ae294f83ad79d76c690c2e4
Samid77 <Dimas@Inass-Mac-Air.local>
2023-01-21 16:23:31 +0700

    Create fundamental routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {logo} from './assets'
+import {Home, CreatePost} from './pages';
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      <h1>Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <header className='w-full flex justify-between bg-white items-center sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to='/'>
+          <img 
+            src={logo}
+            alt={'OpenAI Logo'}
+            className='w-28 object-contain'
+          />
+        </Link>
+        <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white py-2 px-4 rounded-md'>Create</Link>
+      </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f6fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/create-post' element={<CreatePost />}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
